5408b8f37bed2da6322eef12a1e35c70db4619e1
ameliechamp <ac2242@kent.ac.uk>
2024-10-25 11:32:08 +0000

    haudhqisuf

--- WhileLoops.py
+++ WhileLoops.py
@@ -1,1 +1,12 @@
 ### Part Two -- your code goes here. 
+import random
+num = random.randint(0,100)
+guess = int(input("I am thinking of a number. Enter in your guess: "))
+while guess != num:
+    print("That is incorrect.")
+    if guess > num:
+        print("That guess is too high.")
+    elif guess < num:
+        print("That guess is too low.")
+    guess = int(input("Enter another guess: "))
+print("Correct! Well done, the number is", num)
--- forLoop.py
+++ forLoop.py
@@ -1,1 +1,3 @@
 ### Part One -- your code goes here. 
+for i in range(1,11):
+    print("The square of", i, "is", i**2)
