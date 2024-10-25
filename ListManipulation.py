8b24e89b393e07c1dd6554ca5ce1a0d1278f1c7e
ameliechamp <ac2242@kent.ac.uk>
2024-10-25 12:02:56 +0000

    jhqjidv

--- ListManipulation.py
+++ ListManipulation.py
@@ -1,1 +1,9 @@
 ### Part Three -- your code goes here. 
+list = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+sorted = list.sort()
+for i in list:
+    if i == 1:
+        list.remove(1)
+list2 = [7,8]
+add = list.append(list2)
+print(list)
