6cb302734208394705424ba8499297ad932c6d5a
ameliechamp <ac2242@kent.ac.uk>
2024-10-27 14:46:22 +0000

    hxfigweyg

--- AllTogether.py
+++ AllTogether.py
@@ -1,13 +1,29 @@
 ### Part Four -- your code goes here. 
 import random
-index = 0
+
+#create empty list
 list = []
-while index != 10:
+
+#add 10 random numbers to the list
+while len(list) < 10:
     list.append(random.randint(0,100))
-    index += 1
-print(list)
+
+#iterate through the values in the list
+print("\nList values:",end=': ')
 for i in list:
-    while i % 2 == 0:
-        print(i)
-        #list.pop(i)
-#print(list)
+    print(i,end=',')
+
+# remove the even numbers
+i=0
+while i<len(list):
+    if (list[i] % 2) == 0:
+        list.pop(i)
+        #keep index the same
+    else:
+        # move on to the next element    
+        i+=1
+
+#iterate through the new values in the list
+print("\nList new values:",end=': ')
+for i in list:
+    print(i,end=',')
